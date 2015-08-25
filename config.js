74f5a465db0edcd39a4ba809a7f1e06857e43c6b
Ivaylo Arnaudov <ivaylo.arnaudov12@gmail.com>
2015-08-25 23:58:29 +0300

    Configured SystemJS (with ES6 modules) and added a sample module

---
+++ config.js
@@ -0,0 +1,13 @@
+System.config({
+	transpiler: "babel",
+	babelOptions: {
+		optional: [
+			"runtime"
+		]
+	},
+	map: {
+		babel: 'node_modules/babel-core/browser.js',
+		jquery: 'bower_components/jquery/dist/jquery.js',
+		test: '/scripts/test.js'
+	}
+});
---
+++ scripts/test.js
@@ -0,0 +1,13 @@
+function sum(a, b) {
+    return a + b;
+}
+
+function fib(n) {
+   if (n < 2) {
+     return 1;
+   } else {
+     return fib(n - 2) + fib(n - 1);
+   }
+}
+
+export { sum, fib };
