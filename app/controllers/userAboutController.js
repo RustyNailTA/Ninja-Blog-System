57e22e0e2144d04960e1678ba0eafc78b03c6abe
Ivo Paounov <ivo.paunov@gmail.com>
2015-09-04 18:03:48 +0300

    user about mail button, footer mobile device bug fix

--- app/controllers/userAboutController.js
+++ app/controllers/userAboutController.js
@@ -8,7 +8,11 @@ function userAboutController(username, app) {
             localStorage.setItem('blog', JSON.stringify({username: user.username, name: user.name}));
 
             controller.navbarController();
-            templateHandler.loadDataTemplate('templates/user-about.html', '#template-container', user)
+            templateHandler.loadDataTemplate('templates/user-about.html', '#template-container', user);
+            $('#about-email-label').hide();
+            $('#about-email-info').on('click', function () {
+                $('#about-email-label').toggle(500);
+            })
         } else {
             app.notFound();
         }
